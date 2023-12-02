2a2599eb90d9c81c2297a3229d853a04280b5db2
unknown <mhalder9555@gmail.com>
2023-12-01 22:23:10 -0600

    Finished seeding.

---
+++ routes/api/user.api.routes.js
@@ -0,0 +1,70 @@
+const router = require("express").Router();
+const User = require("../../models/User");
+
+const Model = User;
+
+
+// Get all records
+router.get("/", async (req, res) => {
+  try {
+    const payload = await Model.find();
+    res.status(200).json({ status: "success", payload });
+  } catch (err) {
+    res.status(500).json({ status: "error", payload: err.message });
+  }
+})
+
+// Get one record by pk
+router.get("/:id", async (req, res) => {
+  try {
+    const payload = await Model.findByPk(req.params.id);
+    res.status(200).json({ status: "success", payload });
+  } catch (err) {
+    res.status(500).json({ status: "error", payload: err.message });
+  }
+})
+
+// Create a new record
+// router.post("/", async (req, res) => {
+//   try {
+//     const payload = await Model.create(req.body);
+//     res.status(200).json({ status: "success", payload });
+//   } catch (err) {
+//     res.status(500).json({ status: "error", payload: err.message });
+//   }
+// })
+
+// // Update a new record
+// router.put("/:id", async (req, res) => {
+//   try {
+//     const payload = await Model.update(
+//       req.body,
+//       {
+//         where: {
+//           id: req.params.id
+//         }
+//       }
+
+//     );
+//     res.status(200).json({ status: "success", payload });
+//   } catch (err) {
+//     res.status(500).json({ status: "error", payload: err.message });
+//   }
+// })
+
+// // Delete a record
+// router.delete("/:id", async (req, res) => {
+//   try {
+//     const payload = await Model.destroy({
+//       where: {
+//         id: req.params.id
+//       }
+//     });
+//     res.status(200).json({ status: "success" });
+//   } catch (err) {
+//     res.status(500).json({ status: "error", payload: err.message });
+//   }
+// })
+
+
+module.exports = router;
---
+++ utils/data.js
@@ -0,0 +1,71 @@
+const users = [
+  {
+    username: "Wortim",
+    email: "ConstanceJMayon@teleworm.us",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "Thicho",
+    email: "PhoebeNFagan@armyspy.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "Difusleas",
+    email: "CharlesGDube@jourrapide.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "Wastand93",
+    email: "MarkMPena@jourrapide.com",
+    thoughts: [],
+    friends: [],
+  },
+];
+
+const reactions = [
+  {
+    reactionBody: "Cool!",
+    username: "Wortim"
+  },
+  {
+    reactionBody: "Not Cool!",
+    username: "Thicho"
+  },
+  {
+    reactionBody: "Kinda Cool!",
+    username: "Difusleas"
+  },
+  {
+    reactionBody: "Hot!",
+    username: "Wastand93"
+  },
+];
+
+const thoughts = [
+  {
+    thoughtText: "All you need to do is pick up the pen and begin.",
+    username: "Wortim",
+    reactions: reactions
+  },
+  {
+    thoughtText: "All you need to do is pick up the pen and begin.",
+    username: "Thicho",
+    reactions: reactions
+  },
+  {
+    thoughtText: "All you need to do is pick up the pen and begin.",
+    username: "Difusleas",
+    reactions: reactions
+  },
+  {
+    thoughtText: "All you need to do is pick up the pen and begin.",
+    username: "Wastand93",
+    reactions: reactions
+  },
+];
+
+
+module.exports = { users, thoughts };
