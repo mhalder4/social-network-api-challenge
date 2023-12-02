4c4c64d5b96e9312fb6ef291ad87a89ddd53afda
unknown <mhalder9555@gmail.com>
2023-12-01 20:58:54 -0600

    Finished model schemas.

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+const validator = require('validator');
+
+// Schema to create user model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: {
+        validator: function (arr) {
+          return validator.isEmail(arr);
+        },
+        message: "Need a valid email"
+      }
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+// Create a virtual property `friendCount` that gets the amount of friends per post
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(routes);
+
+const date = new Date();
+console.log(date.toLocaleString());
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Now running on port http://localhost:${PORT}!`);
+  });
+});
